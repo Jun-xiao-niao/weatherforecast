8ba546a4a4b36f254d6b0c285c5c4e0e32f0786d
Junxiaoniao <junxiaoniao@gmail.com>
2022-01-18 17:58:09 +0800

    {[>.<]}

--- weather_report.sql
+++ weather_report.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 18/01/2022 11:19:47
+ Date: 18/01/2022 17:57:52
 */
 
 SET NAMES utf8mb4;
@@ -26,14 +26,14 @@ CREATE TABLE `city`  (
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_german2_ci NOT NULL COMMENT '城市名',
   `lon` varchar(255) CHARACTER SET utf8 COLLATE utf8_german2_ci NOT NULL COMMENT '城市经度',
   `lat` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '城市纬度',
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`name`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_german2_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of city
 -- ----------------------------
-INSERT INTO `city` VALUES (101010100, '北京', '116.40528', '39.90498');
 INSERT INTO `city` VALUES (101020100, '上海', '121.47264', '31.23170');
+INSERT INTO `city` VALUES (101010100, '北京', '116.40528', '39.90498');
 INSERT INTO `city` VALUES (101230101, '福州', '119.30623', '26.07530');
 
 -- ----------------------------
@@ -55,11 +55,11 @@ CREATE TABLE `weather`  (
 -- ----------------------------
 INSERT INTO `weather` VALUES (100, '北京', '2022-01-18', '3', '-7', '晴');
 INSERT INTO `weather` VALUES (101, '北京', '2022-01-19', '0', '-8', '多云');
-INSERT INTO `weather` VALUES (102, '北京', '2022-01-20', '0', '-7', '多云');
+INSERT INTO `weather` VALUES (102, '北京', '2022-01-20', '-1', '-7', '多云');
 INSERT INTO `weather` VALUES (200, '上海', '2022-01-18', '11', '5', '晴');
 INSERT INTO `weather` VALUES (201, '上海', '2022-01-19', '14', '5', '多云');
 INSERT INTO `weather` VALUES (202, '上海', '2022-01-20', '10', '4', '多云');
-INSERT INTO `weather` VALUES (300, '福州', '2022-01-18', '13', '11', '小雨');
+INSERT INTO `weather` VALUES (300, '福州', '2022-01-18', '13', '10', '阴');
 INSERT INTO `weather` VALUES (301, '福州', '2022-01-19', '18', '11', '多云');
 INSERT INTO `weather` VALUES (302, '福州', '2022-01-20', '16', '11', '阴');
 
